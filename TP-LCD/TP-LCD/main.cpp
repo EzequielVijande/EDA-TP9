6072ac8bd1e36fbffbfa4b7a36edebb4775fda04
mlarroque <mlarroque@itba.edu.ar>
2017-11-01 15:30:46 -0300

    agregue funciones de matias.cpp a HitachiLCD y  a main

--- TP-LCD/TP-LCD/main.cpp
+++ TP-LCD/TP-LCD/main.cpp
@@ -7,6 +7,42 @@ using namespace std;
 
 int main(void)
 {
+	HitachiLCD lcd;
+	char c = 0;
+	do
+	{
+		scanf(&c);
+		switch (c)
+		{
+		case 'd':
+			lcdMoveCursorRight();
+			break;
+		case 'w':
+			lcdMoveCursorUp();
+			break;
+		case 'a':
+			lcdMoveCursorLeft();
+			break;
+		case 'b':
+			lcdMoveCursorRight();
+			break;
+		case 'c':
+			lcdClear();
+			break;
+		case 'x':
+			lcdClearToEOL();
+			break;
+		case 'g':
+			lcdGetCursorPosition();
+			break;
+		default:
+			lcd << c;
+		}
+	} while (c != 'q');
+	lcd << "demo de c_string";
+	sleep(3);
+	string str = "demo de str_c++";
+	lcd << str;
 
 	return 0;
 }
